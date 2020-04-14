59fe521a11afac30dc58da1adb4e88926d56cb22
Dennys Jun Takao <dennys.takao@gmail.com>
2020-04-14 17:33:24 -0300

    AvaliacaoTecnicaConsole1: Resolucao

--- Teste.Dennys_Jun_Takao/Teste.Dennys_Jun_Takao.ConsoleApp/Program.cs
+++ Teste.Dennys_Jun_Takao/Teste.Dennys_Jun_Takao.ConsoleApp/Program.cs
@@ -3,14 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Teste.Dennys_Jun_Takao.Service.Services;
 
 namespace Teste.Dennys_Jun_Takao.ConsoleApp
 {
     class Program
     {
+        public Program()
+        {
+        }
+
+        ConsoleResolution _cr = new ConsoleResolution();
+
         static void Main(string[] args)
         {
+            Program p = new Program();
+            string nome, sobreNome;
+
+            Console.WriteLine("Digite um Nome:");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Digite um SobreNome:");
+            sobreNome = Console.ReadLine();
+
+            p.AvaliacaoTecnica1(nome, sobreNome);
+
+            Console.ReadKey();
 
         }
+
+        #region Metodos a serem implementados
+        private void AvaliacaoTecnica1(string nome, string sobreNome)
+        {
+            //Escreva uma função que imprima os números de 1 a 100 e, ao lado dos números múltiplos de 3 imprima “<Nome>”, ao lado dos múltiplos de 5 imprima “<SobreNome>” e ao lado dos múltiplos de 3 e 5 imprima "<Nome><SobreNome>"”.
+            Console.WriteLine("Avaliação técnica 1");
+            var numList = _cr.AvaliacaoTecnica1(nome, sobreNome);
+            foreach (var item in numList)
+                Console.WriteLine(item);
+        }
+        #endregion
     }
 }
---
+++ Teste.Dennys_Jun_Takao/Teste.Dennys_Jun_Takao.Service/Services/ConsoleResolution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste.Dennys_Jun_Takao.Service.Services
+{
+    public class ConsoleResolution
+    {
+        public List<string> AvaliacaoTecnica1(string nome, string sobreNome)
+        {
+            List<string> retorno = new List<string>();
+
+            for (int i = 1; i <= 100; i++)
+                if (i % 3 == 0 && i % 5 == 0)
+                    retorno.Add(i + nome + sobreNome);
+                else
+                    if (i % 3 == 0)
+                    retorno.Add(i + nome);
+                else
+                    if (i % 5 == 0)
+                    retorno.Add(i + sobreNome);
+                else
+                    retorno.Add(i.ToString());
+
+            return retorno;
+        }
+    }
+}
